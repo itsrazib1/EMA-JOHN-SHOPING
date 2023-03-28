c82e91e540d7fa04fc5bf0f6b6e1def30e363b20
RAZiB <xoxorazibahamed@gmail.com>
2023-03-28 13:16:05 +0600

    added

--- src/Component/Header/header.jsx
+++ src/Component/Header/header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <nav className="header">
       <img src={logo} alt="" />
       <div>
-        <a href="/Order">Order</a>
-        <a href="/Order Review">Order Review</a>
-        <a href="/Manage Inventory">Manage Inventory</a>
+        <a href="./Order">Order</a>
+        <a href="./Order Review">Order Review</a>
+        <a href="./Manage Inventory">Manage Inventory</a>
         <a href="Login">Login</a>
       </div>
     </nav>
